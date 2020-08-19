9045390efd9d1d6ca1f923e55f79b2cd7572f582
abhishek <abhishekanantharam123@gmail.com>
2020-08-19 10:06:08 +0000

    Add Q route

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/',function(req,res){
 	res.send("Welcome to HOME PAGE");
 })
 
+app.get('/questions',function(req,res){
+	res.send('Route to view all the Questions');
+})
+
 app.listen(3000,function(){
 	console.log("Server Started");
 })
